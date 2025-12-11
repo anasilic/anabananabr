d3baaa2cd3ce95f09b616e29b279dd76d98acdeb
ana.silić <ana.sili@KABINF06>
2025-12-11 19:19:37 +0100

    nastavak jos maloooo do krajaaa

--- anabanananana/Form2.cs
+++ anabanananana/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,22 @@ namespace anabanananana
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            line = textBox1 + "|" + textBox2 + "|" + textBox3 + "|" + textBox4;
+            StreamWriter sw = new StreamWriter("imena.txt");
+            sw.WriteLine(line);
+            sw.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            MessageBox.Show("Podatci su spremljeni!");
+
+
         }
     }
 }
